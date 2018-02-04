0adbeb13a81144246f71dfb9456eb7f7339c1283
Gildas HERY <ghery@e2r6p13.42.fr>
2018-02-04 09:28:22 +0100

    menu button in menu

--- Menu.js
+++ Menu.js
@@ -1,1 +1,22 @@
+function AirOnClick(){
+    game.state.start('Airstate');
+}
 
+function WaterOnClick(){
+    game.state.start('Waterstate');
+}
+
+function EarthOnClick(){
+    game.state.start('Earthstate');
+}
+
+function FireOnClick(){
+   game.state.start('Firestate');
+}
+
+
+function ClickEvent()
+{
+    console.log("click event");
+    click = 1;
+}
--- hunter.js
+++ hunter.js
@@ -1116,28 +1116,7 @@ game.state.add('Earthstate', Earthstate);
 game.state.add('Firestate', Firestate);
 
 game.state.start('bootstate');
-function AirOnClick(){
-    game.state.start('Airstate');
-}
 
-function WaterOnClick(){
-    game.state.start('Waterstate');
-}
-
-function EarthOnClick(){
-    game.state.start('Earthstate');
-}
-
-function FireOnClick(){
-   game.state.start('Firestate');
-}
-
-
-function ClickEvent()
-{
-    console.log("click event");
-    click = 1;
-}
 
 function preload() {
 
